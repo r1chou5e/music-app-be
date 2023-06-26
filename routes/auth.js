e6f2439a6f2a470a1e07333af6ae0bd62b8e6e1f
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-27 02:53:00 +0700

    update auth_time when login if user exists

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,9 @@ router.get("/login", async (req, res) => {
       // checking user exists or not
       const userExists = await user.findOne({ user_id: decodeValue.user_id });
       if (!userExists) {
-        newUserData(decodeValue, req, res);
+        createNewUserData(decodeValue, req, res);
       } else {
-        return res.send("Need to update");
+        updateNewUserData(decodeValue, req, res);
       }
     }
   } catch (error) {
@@ -28,7 +28,7 @@ router.get("/login", async (req, res) => {
   }
 });
 
-const newUserData = async (decodeValue, req, res) => {
+const createNewUserData = async (decodeValue, req, res) => {
   const newUser = new user({
     name: decodeValue.name,
     email: decodeValue.email,
@@ -42,6 +42,26 @@ const newUserData = async (decodeValue, req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(200).send({ user: savedUser });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error });
+  }
+};
+
+const updateNewUserData = async (decodeValue, req, res) => {
+  const filter = { user_id: decodeValue.user_id };
+
+  const options = {
+    upsert: true,
+    new: true,
+  };
+
+  try {
+    const result = await user.findOneAndUpdate(
+      filter,
+      { auth_time: decodeValue.auth_time },
+      options
+    );
+    res.status(200).send({ user: result });
   } catch (error) {
     res.status(400).send({ success: false, msg: error });
   }
