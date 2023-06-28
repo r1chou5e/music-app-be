e72b2b078489bc5a005cebfdc60bf658d2675d42
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-28 20:10:39 +0700

    get song by id, update and delete song

--- routes/song.js
+++ routes/song.js
@@ -36,4 +36,76 @@ router.get("/get-all", async (req, res) => {
   }
 });
 
+router.get("/get/:id", async (req, res) => {
+  const filter = { _id: req.params.id };
+
+  const data = await song.findOne(filter);
+
+  if (data) {
+    return res.status(200).send({ success: true, song: data });
+  } else {
+    return res.status(400).send({ success: false, msg: "Song not found." });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const filter = {
+    _id: req.params.id,
+  };
+
+  try {
+    const result = await song.deleteOne(filter);
+    return res
+      .status(200)
+      .send({ success: true, msg: "Deleted successfully.", result: result });
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ success: false, msg: "Failed to delete.", error: error });
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  const filter = {
+    _id: req.params.id,
+  };
+
+  const options = {
+    new: true,
+  };
+
+  const update = {
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+    songUrl: req.body.songUrl,
+    album: req.body.album,
+    artist: req.body.artist,
+    language: req.body.language,
+    category: req.body.category,
+  };
+
+  try {
+    const updatedSong = await song.findOneAndUpdate(filter, update, options);
+
+    if (updatedSong) {
+      return res.status(200).send({
+        success: true,
+        msg: "Updated Successfully.",
+        data: updatedSong,
+      });
+    } else {
+      return res.status(400).send({
+        success: false,
+        msg: "Song not found.",
+      });
+    }
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ success: false, msg: "Failed to update.", error: error });
+  }
+});
+
+
+
 module.exports = router;
