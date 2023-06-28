efd19a731b614e5f8a02ef9511d4b463319694ee
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-28 17:00:02 +0700

    save and get-all album

--- routes/album.js
+++ routes/album.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
 
-router.get("/getAll", async (req, res) => {
-  return res.json("getting all albums");
+const album = require("../models/album");
+
+router.post("/save", async (req, res) => {
+  const newAlbum = album({
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+  });
+
+  try {
+    const savedAlbum = await newAlbum.save();
+    return res.status(200).send({ success: true, album: savedAlbum });
+  } catch (error) {
+    return res.status(400).send({ success: false, msg: error });
+  }
+});
+
+router.get("/get/:id", async (req, res) => {
+  const filter = { _id: req.params.id };
+
+  const data = await album.findOne(filter);
+
+  if (data) {
+    return res.status(200).send({ success: true, album: data });
+  } else {
+    return res.status(400).send({ success: false, msg: "Album not found." });
+  }
 });
 
 module.exports = router;
