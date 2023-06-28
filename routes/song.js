50e7bdeeb69e22ee9cee6076eab73d51f760600b
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-06-28 20:02:15 +0700

    save song and get all song

--- routes/song.js
+++ routes/song.js
@@ -1,7 +1,39 @@
 const router = require("express").Router();
 
-router.get("/getAll", async (req, res) => {
-  return res.json("getting all songs");
+const song = require("../models/song");
+
+router.post("/save", async (req, res) => {
+  const newSong = song({
+    name: req.body.name,
+    imageUrl: req.body.imageUrl,
+    songUrl: req.body.songUrl,
+    album: req.body.album,
+    artist: req.body.artist,
+    language: req.body.language,
+    category: req.body.category,
+  });
+
+  try {
+    const savedSong = await newSong.save();
+    return res.status(200).send({ success: true, song: savedSong });
+  } catch (error) {
+    return res.status(400).send({ success: false, msg: error });
+  }
+});
+
+router.get("/get-all", async (req, res) => {
+  const options = {
+    sort: {
+      createdAt: 1,
+    },
+  };
+
+  const data = await song.find({}, null, options);
+  if (data) {
+    return res.status(200).send({ success: true, songs: data });
+  } else {
+    return res.status(400).send({ success: false, msg: "Song not found." });
+  }
 });
 
 module.exports = router;
